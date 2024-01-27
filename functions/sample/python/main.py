b33a9c53d3fe81b353cbcdf3cf15eb86f11829c5
VSNair <vsn0747@gmail.com>
2024-01-27 03:22:01 -0500

    Submit

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -8,28 +8,17 @@ from cloudant.error import CloudantException
 import requests
 
 
-def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
+async function main(params) {
+     const cloudant = Cloudant({
+         url: params.COUCH_URL,
+         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+     });
+ 
+ 
+     try {
+         let dbList = await cloudant.db.list();
+         return { "dbs": dbList };
+     } catch (error) {
+         return { error: error.description };
+     }
+ }
